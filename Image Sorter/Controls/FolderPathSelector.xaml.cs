525c7cdb6ba43f04032dca9158a8ede8163c2a39
MartinNN07 <martinnedev07@gmail.com>
2023-12-27 09:53:26 +0200

    first commit

---
+++ Image Sorter/Controls/FolderPathSelector.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Image_Sorter.Controls
+{
+    public partial class FolderPathSelector : UserControl, INotifyPropertyChanged
+    {
+        public FolderPathSelector()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private string defaultText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string DefaultText
+        {
+            get 
+            { 
+                return defaultText;
+            }
+            set 
+            { 
+                defaultText = value;
+                onPropertyChanged();
+            }
+        }
+
+
+        private void btnChoosePath_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog openFolderDialog = new OpenFolderDialog();
+            //start at 'This PC'
+            openFolderDialog.InitialDirectory = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}";
+            if (openFolderDialog.ShowDialog() == null)
+            {
+                return;
+            }
+
+            tblockDefaultText.Visibility = Visibility.Hidden;
+            labelFolderPath.Content = openFolderDialog.FolderName;
+            if (string.IsNullOrEmpty(openFolderDialog.FolderName)) 
+            {
+                tblockDefaultText.Visibility = Visibility.Visible;
+            }
+            
+
+        }
+
+        private void onPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ Image Sorter/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Image_Sorter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
